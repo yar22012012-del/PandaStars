b3d8e7a9b891951f6e606243acba573dfd97f916
TBG <yar22012012@gmail.com>
2026-07-16 01:46:45 +0300

    added games logic

--- script.js
+++ script.js
@@ -1,131 +1,19 @@
-let balance =
-Number(localStorage.getItem("balance")) || 100;
+let stars = 100;
 
-
-let opened =
-Number(localStorage.getItem("opened")) || 0;
-
-
-
-update();
-
-
-
-
-
-function changeScreen(id){
-
-
-document.querySelectorAll(".screen")
-.forEach(x=>{
-
-x.classList.remove("active");
-
-});
-
-
-
-document
-.getElementById(id)
-.classList.add("active");
-
-
-}
-
-
-
-
-
-
-
-const rewards=[
-
-
-{
-name:"⭐⭐ 2 Stars",
-value:2,
-chance:45
-},
-
-
-{
-name:"⭐⭐⭐ 3 Stars",
-value:3,
-chance:35
-},
-
-
-{
-name:"⭐ 1 Star",
-value:1,
-chance:8
-},
-
-
-{
-name:"⭐⭐⭐⭐⭐ 5 Stars",
-value:5,
-chance:7
-},
-
-
-{
-name:"⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 11 Stars",
-value:11,
-chance:3
-},
-
-
-{
-name:"⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 17 Stars",
-value:17,
-chance:1.9
-},
-
-
-{
-name:"🎁 Telegram NFT",
-value:300,
-chance:.1
-}
-
-
-];
-
-
-
-
-
-
+let lastCase = 0;
 
 
 function openCase(){
 
 
-
-let last =
-localStorage.getItem("caseTime");
+let now = Date.now();
 
 
 
-let now =
-Date.now();
+if(now - lastCase < 86400000){
 
 
-
-
-if(last){
-
-
-let left =
-86400000-(now-last);
-
-
-
-if(left>0){
-
-
-showTimer(left);
+alert("Кейс доступен раз в 24 часа");
 
 
 return;
@@ -134,83 +22,55 @@ return;
 }
 
 
-}
+
+let random = Math.random()*100;
+
+
+let reward;
 
 
 
+if(random < 45){
 
-
-
-let random =
-Math.random()*100;
-
-
-let total=0;
-
-
-let win;
-
-
-
-for(let r of rewards){
-
-
-total+=r.chance;
-
-
-if(random<=total){
-
-win=r;
-
-break;
+reward = 2;
 
 }
 
+else if(random < 80){
+
+reward = 3;
 
 }
 
+else if(random < 88){
 
+reward = 1;
 
+}
 
-balance+=win.value;
+else if(random < 95){
 
+reward = 5;
 
-opened++;
+}
 
+else if(random < 98){
 
+reward = 11;
 
-localStorage.setItem(
-"balance",
-balance
-);
+}
 
+else if(random < 99.9){
 
-localStorage.setItem(
-"opened",
-opened
-);
+reward = 17;
 
+}
 
-localStorage.setItem(
-"caseTime",
-now
-);
+else{
 
+alert("🎁 Тебе выпал NFT подарок!");
 
-
-
-document.getElementById("result")
-.innerHTML=
-
-"🎉 Выпало:<br>"+win.name;
-
-
-
-update();
-
-
-startTimer();
-
+reward = 300;
 
 }
 
@@ -218,102 +78,24 @@ startTimer();
 
 
 
-
-
-function startTimer(){
-
-
-setInterval(()=>{
-
-
-let last =
-localStorage.getItem("caseTime");
-
-
-if(!last)return;
+stars += reward;
 
 
 
-let left =
-86400000-(Date.now()-last);
+document.getElementById("balance").innerHTML = stars;
+
+
+document.getElementById("profileBalance").innerHTML = stars;
 
 
 
-if(left<=0){
+document.getElementById("result").innerHTML =
 
-
-document.getElementById("timer")
-.innerHTML=
-"Доступен сейчас";
-
-
-return;
-
-
-}
+"🎉 Ты выиграл ⭐"+reward;
 
 
 
-showTimer(left);
-
-
-
-},1000);
-
-
-}
-
-
-
-
-
-
-function showTimer(ms){
-
-
-let h =
-Math.floor(ms/3600000);
-
-
-let m =
-Math.floor(ms%3600000/60000);
-
-
-let s =
-Math.floor(ms%60000/1000);
-
-
-
-document.getElementById("timer")
-.innerHTML=
-
-"Следующее открытие: "
-+h+":"+
-m+":"+
-s;
-
-
-}
-
-
-
-
-
-
-
-function update(){
-
-
-document.getElementById("balance")
-.innerHTML=balance;
-
-
-document.getElementById("profileBalance")
-.innerHTML=balance;
-
-
-document.getElementById("opened")
-.innerHTML=opened;
+lastCase = now;
 
 
 }
