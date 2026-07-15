1306c08a78ec07de9cd4f8430f3d118ac07f1903
TBG <yar22012012@gmail.com>
2026-07-15 18:27:42 +0300

    new panda stars design

--- script.js
+++ script.js
@@ -1,5 +1,98 @@
-let tg = window.Telegram.WebApp;
+function openPage(page){
 
-tg.expand();
 
-console.log("Mini App запущено");
+let app = document.getElementById("app");
+
+
+
+if(page === "cases"){
+
+
+app.innerHTML = `
+
+<h1>
+🎁 Кейсы
+</h1>
+
+<p>
+Открывай кейсы за ⭐
+</p>
+
+`;
+
+}
+
+
+
+
+if(page === "mine"){
+
+
+app.innerHTML = `
+
+<h1>
+⛏ Шахта
+</h1>
+
+<p>
+Ищи звезды и множители
+</p>
+
+`;
+
+}
+
+
+
+
+
+if(page === "profile"){
+
+
+app.innerHTML = `
+
+<h1>
+👤 Профиль
+</h1>
+
+<p>
+Баланс: 0⭐
+</p>
+
+`;
+
+}
+
+
+
+
+
+if(page === "top"){
+
+
+app.innerHTML = `
+
+<h1>
+🏆 Топ игроков
+</h1>
+
+<p>
+
+1. Alex - 1000⭐
+
+<br>
+
+2. Max - 500⭐
+
+<br>
+
+3. Ivan - 250⭐
+
+</p>
+
+`;
+
+}
+
+
+}
