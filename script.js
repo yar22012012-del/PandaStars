8ccac3b0ca63248e02adf690b0bf192d5d9448fa
TBG <yar22012012@gmail.com>
2026-07-16 01:25:08 +0300

    created app structure

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-function openCase(){
+function showPage(id){
 
 
-alert("Бесплатный кейс");
+let pages=document.querySelectorAll(".page");
+
+
+pages.forEach(page=>{
+
+page.classList.remove("active");
+
+});
+
+
+
+document
+.getElementById(id)
+.classList.add("active");
+
+
+
+
+let nav=document.querySelectorAll(".nav");
+
+
+nav.forEach(n=>{
+
+n.classList.remove("active");
+
+});
+
+
+}
+
+
+
+
+function openGame(game){
+
+
+showPage(game);
+
+
+}
+
+
+
+
+
+function backGames(){
+
+
+showPage("games");
 
 
 }
